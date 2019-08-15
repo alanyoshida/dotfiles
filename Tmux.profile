0fbb2c741c27cd4aba1989a1d65cc47c2948622c
Alan Yoshida <alanyoshida@msn.com>
2019-08-15 15:50:48 -0300

    Tmux with fish

---
+++ Tmux.profile
@@ -0,0 +1,8 @@
+[Appearance]
+ColorScheme=Linux
+Font=Hack,11,-1,7,50,0,0,0,0,0,Regular
+
+[General]
+Command=/usr/bin/tmux new -A -s Main
+Name=Tmux
+Parent=FALLBACK/
---
+++ config.fish
@@ -0,0 +1,8 @@
+set fish_function_path $fish_function_path "/home/alan.oliveira/.local/lib/python2.7/site-packages/powerline/bindings/fish"
+powerline-setup
+
+alias k="kubectl"
+alias kqa="kubectl config use-context $USER-qa.k8s.dafiti.local"
+alias klive="kubectl config use-context $USER-live.k8s.dafiti.local"
+alias starcraft="flatpak run com.blizzard.WoW"
+alias aliceqa="ssh alan.oliveira@172.17.14.90"
