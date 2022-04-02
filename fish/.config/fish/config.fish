45cb5fcbfd744c5cf5495b7be6ce881aef295a31
Alan Yoshida <alanyoshida@msn.com>
2022-04-02 00:27:10 -0300

    add some envs to fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,10 @@
 # Remove the global version, as it shadows the universal one
 set -eg fish_user_paths
 
+set GOPATH $HOME/go
+set -gx PATH $PATH $HOME/go/bin
+set -x SHELL /bin/bash
+set -gx PATH $PATH $HOME/.krew/bin
+
 # try to launch tmux
 launch-tmux
