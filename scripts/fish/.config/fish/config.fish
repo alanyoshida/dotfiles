4eb471ff33498f3d978f3e522b44ef2dfefc95fa
Alan Yoshida <alanyoshida@msn.com>
2022-04-03 17:44:16 -0300

    add fish integration with kitty

--- scripts/fish/.config/fish/config.fish
+++ scripts/fish/.config/fish/config.fish
@@ -10,5 +10,11 @@ set -gx PATH $PATH /usr/local/go/bin
 set -gx PATH $PATH $HOME/.krew/bin
 set -x SHELL /bin/bash
 
+if set -q KITTY_INSTALLATION_DIR
+    set --global KITTY_SHELL_INTEGRATION enabled
+    source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
+    set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
+end
+
 # try to launch tmux
 launch-tmux
